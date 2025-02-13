d98f168331449afeae43af844b89c480fcffd6e0
user2230 <furkanefeceylan2008@hotmail.com>
2025-02-13 12:57:24 +0100

    first

---
+++ m3prog_project/public/01/variabelennaam.php
@@ -0,0 +1,16 @@
+<?php
+
+$auto2 = "BMW";
+$_naam = "batman";
+//$auto's = "audi";
+//$auto/onderdelen = "uitlaat";
+//$huis&tuin = "schutting";
+$naw = "contactweg";
+//$22 = "getal";
+//$$auto = "merk";
+$kleurVanAuto = "rood";
+//$software-dev = "opleiding";
+
+//echo $variabele;
+
+?>
