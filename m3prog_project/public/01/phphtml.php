94d5f5fbe3ef78592de72a1af8a4eee011cc4cc4
user2230 <furkanefeceylan2008@hotmail.com>
2025-02-13 12:03:18 +0100

    first

---
+++ m3prog_project/public/01/phphtml.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title><?php echo "Titel van de pagina" ?></title>
+    </head>
+    <body>
+        <h2>De datum vandaag is: <?php echo date('d-m-Y')?>, en de tijd is <?php echo date("H:i")?> uur.
+        
+        <p><h2>Mijn favoriete game</h2><?php echo "Super metroid";?></p>
+    </body>
+</html> 
