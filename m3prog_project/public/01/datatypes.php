2a956608d93312be7abe3ff90c3a1216895f5e39
user2230 <furkanefeceylan2008@hotmail.com>
2025-02-13 13:11:17 +0100

    first

---
+++ m3prog_project/public/01/datatypes.php
@@ -0,0 +1,18 @@
+<?php
+
+$huisnummer = 23;
+$geslaagd = false;
+$benzinePrijs = 2.10;
+
+echo "huisnummers";
+echo "<br>";
+echo $huisnummer;
+echo "<br>";
+
+echo "geslaagd of niet";
+echo $geslaagd;
+echo "<br>";
+
+echo "benzine prijs";
+echo $benzinePrijs;
+echo "<br>";
---
+++ m3prog_project/public/01/showtypes.php
@@ -0,0 +1,18 @@
+<?php
+
+$nummer = 89;
+$salaris = 0.5;
+$zin = "hello zin";
+$waarOfNiet =false;
+
+$rijtje = [1,2,3,4,5];
+
+echo gettype($nummer);
+echo "<br>";
+echo gettype ($salaris);
+echo "<br>";
+echo gettype($zin);
+echo "<br>";
+echo gettype($waarOfNiet);
+echo "<br>";
+echo gettype(rijtje);
