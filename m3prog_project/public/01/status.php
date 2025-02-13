4cb9ea7fd1ca4874e2083a60ef95de167a207dfd
user2230 <furkanefeceylan2008@hotmail.com>
2025-02-13 12:47:54 +0100

    first

---
+++ m3prog_project/public/01/status.php
@@ -0,0 +1,45 @@
+<?php
+
+$status = "offline";
+echo "$status";
+echo "<br>";
+$status = "available";
+echo "$status";
+echo "<br>";
+$status = "busy";
+echo "$status";
+echo "<br>";
+$status = "do not disturb";
+echo "$status";
+echo "<br>";
+$status = "offline";
+echo "$status";
+echo "<br>";
+echo "<br>";
+
+
+$prijsNaam = "prijs";
+echo "$prijsNaam";
+echo "<br>";
+
+$prijs = 20.5;
+echo "$prijs";
+echo "<br>";
+
+$prijs = 242.4;
+echo "$prijs";
+echo "<br>";
+
+$prijs = 837;
+echo "$prijs";
+echo "<br>";
+
+
+$prijs = 386;
+echo "$prijs";
+echo "<br>";
+
+$prijs = 5.7;
+echo "$prijs";
+
+?>
