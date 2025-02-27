5fb5495bd7db500e1ba068076f7721264b3d6868
user2230 <furkanefeceylan2008@hotmail.com>
2025-02-27 12:40:01 +0100

    mensen

---
+++ m3prog_project/public/02/variabelen.php
@@ -0,0 +1,18 @@
+<?php
+$stad = "Amsterdam";
+$hetWeer = "zonnig";
+$aantalMensen = 150000;
+$detemperatuur = 23.4;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?= "<h1> Het is vandaag $hetWeer in $stad en het heeft $aantalMensen in $stad met de $detemperatuur</h1>";?>
+</body>
+</html>
