80883f79f4e624e81ad9f77ef992bed929aeb8c6
user2230 <furkanefeceylan2008@hotmail.com>
2025-02-27 12:11:40 +0100

    plak

--- m3prog_project/public/02/plak.php
+++ m3prog_project/public/02/plak.php
@@ -9,3 +9,20 @@ echo $extraString;
 
 $fullname = "Furkan" ." ". "Ceylan" . "<br>";
 echo $fullname;
+
+$naam = "Yoshi";
+$ondertekentDoor = "directeur bowser";
+
+$enemy = "Koopa Troopa";
+
+$emailTekst = "Beste $naam <br>
+Helaas moeten wij uw verzoek afwijzen.<br>
+onze $enemy vinden het niet leuk om opgegeten te worden<br>
+<br>
+Hoogachtend<br>
+$ondertekentDoor <br>
+<br>
+PS:<br>
+De princess is in haar kasteel... voor nu :>";
+echo $emailTekst;
+
