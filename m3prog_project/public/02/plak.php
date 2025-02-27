37c62cf43889d156bf7aabd06e92f20a2ac69919
user2230 <furkanefeceylan2008@hotmail.com>
2025-02-27 11:58:57 +0100

    first

---
+++ m3prog_project/public/02/plak.php
@@ -0,0 +1,11 @@
+<?php
+
+$standaardString = 'hello world<br/>';
+$extraString = "extra world<br/>";
+
+echo $standaardString;
+echo $extraString;
+
+
+$fullname = "Furkan" ." ". "Ceylan" . "<br>";
+echo $fullname;
