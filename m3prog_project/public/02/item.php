9f21285d0762c47663b55f325ecadf95d9249831
user2230 <furkanefeceylan2008@hotmail.com>
2025-02-27 12:59:41 +0100

    commit

---
+++ m3prog_project/public/02/item.php
@@ -0,0 +1,24 @@
+<?php
+
+$modifiers = "Magic Damage Reduced By 3";
+$modifiers1 = "+50-75 To Attack Rating (varies)";
+$modifiers2 = "Attacker Takes Damage of 3*";
+$modifiers3 = "15-30% Better Chance of Getting Magic Items (varies)";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?= "<h1>Immortal King's pillar</h1>";?>
+    <?= "<p> $modifiers <br>
+    $modifiers1 <br>
+    $modifiers2 <br>
+    $modifiers3 <br></p>";?>
+    
+</body>
+</html>
