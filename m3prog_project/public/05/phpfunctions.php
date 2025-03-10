f7746bba606d69333f3dbd01101c913f4662d337
user2230 <furkanefeceylan2008@hotmail.com>
2025-03-10 12:18:28 +0100

    first

---
+++ m3prog_project/public/05/phpfunctions.php
@@ -0,0 +1,25 @@
+<?php
+
+//vraag 1: wat is het type het argument van strlen?
+//antwoord 1: string
+
+//vraag 2: wat is het type van wat strlen returned?
+//antwoord 1: integer
+
+//vraag 3: het getal wat strlen returned, wat is dat?
+//antwoord 3: het aantal karakters in de string
+
+// vraag 4 : hoevaak heb je nu de function strlen aangeroepen?
+//antwoord 4: 2 keer
+
+
+
+
+$hoeLangIsDitWoord = "scrabbleWoord";
+$woordLengte = strlen($hoeLangIsDitWoord);
+print $woordLengte;
+
+$test = "hoelang";
+$woordLang = strlen($test);
+print $woordLang;
+?>
