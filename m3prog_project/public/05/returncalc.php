d337a8124e84456c01fa92e9403cb7fd23d0ea2e
user2230 <furkanefeceylan2008@hotmail.com>
2025-03-17 12:24:12 +0100

    first

--- m3prog_project/public/05/returncalc.php
+++ m3prog_project/public/05/returncalc.php
@@ -19,23 +19,20 @@ function multiply($getal1, $getal2) {
     return $getal1 * $getal2;
 }
 
-echo "<h2>Optellen</h2>";
-echo "100 + 100 = " . add(100, 100) . "<br>";
-echo "4 + 9 = " . add(4, 9) . "<br>";
-echo "33 + 33 = " . add(33, 33) . "<br>";
-
-echo "<h2>Aftrekken</h2>";
-echo "100 - 50 = " . subtract(100, 50) . "<br>";
-echo "20 - 5 = " . subtract(20, 5) . "<br>";
-echo "10 - 30 = " . subtract(10, 30) . "<br>";
-
-echo "<h2>Delen</h2>";
-echo "100 / 5 = " . divide(100, 5) . "<br>";
-echo "25 / 2 = " . divide(25, 2) . "<br>";
-echo "10 / 0 = " . divide(10, 0) . "<br>";
-
-echo "<h2>Vermenigvuldigen</h2>";
-echo "10 * 10 = " . multiply(10, 10) . "<br>";
-echo "3 * 7 = " . multiply(3, 7) . "<br>";
-echo "6 * 6 = " . multiply(6, 6) . "<br>";
+ "<h2>Optellen</h2>";
+ "100 + 100 = " . add(100, 100) . "<br>";
+ "4 + 9 = " . add(4, 9) . "<br>";
+ "33 + 33 = " . add(33, 33) . "<br>";
+ "<h2>Aftrekken</h2>";
+ "100 - 50 = " . subtract(100, 50) . "<br>";
+ "20 - 5 = " . subtract(20, 5) . "<br>";
+ "10 - 30 = " . subtract(10, 30) . "<br>";
+ "<h2>Delen</h2>";
+ "100 / 5 = " . divide(100, 5) . "<br>";
+ "25 / 2 = " . divide(25, 2) . "<br>";
+ "10 / 0 = " . divide(10, 0) . "<br>";
+ "<h2>Vermenigvuldigen</h2>";
+ "10 * 10 = " . multiply(10, 10) . "<br>";
+ "3 * 7 = " . multiply(3, 7) . "<br>";
+ "6 * 6 = " . multiply(6, 6) . "<br>";
 ?>
