7e0418627b1d5514436fd85f76f974851b6df61a
user2230 <furkanefeceylan2008@hotmail.com>
2025-03-17 12:09:58 +0100

    first

--- m3prog_project/public/05/returns.php
+++ m3prog_project/public/05/returns.php
@@ -1,10 +1,28 @@
 <?php
 
-$mijnFunctionResultaat = mijnFunction();
-print $mijnFunctionResultaat;
+
+
 
 function mijnFunction()
 {
     return 1;
-   
 }
+
+$mijnFunctionResultaat = mijnFunction();
+echo $mijnFunctionResultaat;
+echo "<br/>";
+
+
+function returnVariable()
+{
+    $result = rand();
+    return $result;
+
+}
+print returnVariable();
+echo "<br/>";
+print returnVariable();
+echo "<br/>";
+print returnVariable();
+echo "<br/>";
+
